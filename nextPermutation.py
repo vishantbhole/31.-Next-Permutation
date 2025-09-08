10668871c400ee78cc8000a440bb43d0c6b48547
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-07 20:12:11 -0500

    Update nextPermutation.py

--- nextPermutation.py
+++ nextPermutation.py
@@ -1,2 +1,8 @@
 # 31. Next Permutation
 
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
